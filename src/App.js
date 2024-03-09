6761f19fa5c003d75efee850b99f5c9f05a63c3c
six-shot <okhuomonajayi54@gmail.com>
2024-03-09 22:34:09 +0100

    animation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Routess from './Route';
+import FirstScene from './modules/home/components/firstscene';
 
 function App() {
   return (
     <div>
-    <Routess/>
+      <FirstScene/>
+
     </div>
   );
 }
