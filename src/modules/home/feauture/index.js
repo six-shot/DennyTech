5298dce27dfe346317c1a32e4566d6dc20182702
six-shot <okhuomonajayi54@gmail.com>
2024-03-09 13:14:38 +0100

    animation

--- src/modules/home/feauture/index.js
+++ src/modules/home/feauture/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { staggerContainer, fadeIn, slideIn } from "../data/motion";
+import { PiTooth } from "react-icons/pi";
+import { IconContext } from "react-icons";
 
 export default function Hero() {
   return (
@@ -13,28 +15,96 @@ export default function Hero() {
       <div className="bg-white w-full font-poppins">
         <div className="max-w-[1440px] mx-auto">
           <div className=" px-[10%]  sm:h-[90vh] sm:py-0 py-[3%] flex flex-col  justify-center ">
-            <div className="w-full grid sm:grid-cols-12">
+            <div className="w-full grid sm:grid-cols-12 space-x-5 items-center">
               <div className="col-span-8">
                 <motion.h1
-                  variants={slideIn("right", "tween", 0.2, 1)}
+                  variants={fadeIn("right", "tween", 0.2, 1)}
                   className="sm:text-[80px] text-[30px]  tracking-[2px]"
                 >
                   REVOLUTIONIZING
                 </motion.h1>
               </div>
+              <motion.div
+                variants={fadeIn("down", "tween", 0.8, 1)}
+                className="col-span-4"
+              >
+                <div className="flex space-x-3 items-center">
+                  <div className="w-[30px] h-[30px] flex rounded-full justify-center items-center bg-[#FE4C1A]">
+                    <IconContext.Provider
+                      value={{ color: "white", value: "20px" }}
+                    >
+                      <PiTooth />
+                    </IconContext.Provider>
+                  </div>
+                  <div className="w-[30px] h-[30px] flex rounded-full justify-center items-center bg-[#FE4C1A]">
+                    <IconContext.Provider
+                      value={{ color: "white", value: "20px" }}
+                    >
+                      <PiTooth />
+                    </IconContext.Provider>
+                  </div>
+                  <div className="w-[30px] h-[30px] flex rounded-full justify-center items-center bg-[#FE4C1A]">
+                    <IconContext.Provider
+                      value={{ color: "white", value: "20px" }}
+                    >
+                      <PiTooth />
+                    </IconContext.Provider>
+                  </div>
+                </div>
+                <h5 className="text-left text-sm">
+                  Modern Solutions
+                  <br />
+                  Timeless smiles
+                </h5>
+              </motion.div>
             </div>
-            <div className="w-full grid sm:grid-cols-12 ">
-              <div className="col-span-4 w-full "></div>
-              <div className="col-span-8 w-full ">
+            <div className="w-full grid sm:grid-cols-12 gap-5  items-center">
+              <div className="col-span-3 w-full  ">
+                <motion.div
+                  variants={fadeIn("down", "tween", 0.8, 1)}
+                  className="flex items-center"
+                >
+                  <h5 className="w-[150px] h-[40px] flex justify-center items-center bg-[#D7E8F6] rounded-[30px]">
+                    ASSISTANCE
+                  </h5>
+                  <div className="w-[40px] h-[40px] flex rounded-full justify-center items-center bg-[#FE4C1A]">
+                    <IconContext.Provider
+                      value={{ color: "white", value: "40px" }}
+                    >
+                      <PiTooth className="text-[30px]" />
+                    </IconContext.Provider>
+                  </div>
+                </motion.div>
+              </div>
+              <div className="col-span-3 w-full">
                 <motion.h1
                   variants={fadeIn("right", "tween", 0.2, 1)}
                   className="sm:text-[80px] text-[30px]  tracking-[2px] sm:text-left text-right"
                 >
-                  DENTAL CARE
+                  DENTAL
+                </motion.h1>
+              </div>
+              <motion.div
+                variants={fadeIn("up", "tween", 0.8, 1)}
+                className="col-span-3 w-full"
+              >
+                <div className="flex justify-center items-center">
+                  <div className="relative flex flex-row">
+                    <div className="w-[100px] h-[100px] bg-black rounded-full "></div>
+                    <div className="w-[100px] h-[100px] bg-red-500 rounded-full absolute left-[80%] top-0"></div>
+                  </div>
+                </div>
+              </motion.div>
+              <div className="col-span-3 w-full  ">
+                <motion.h1
+                  variants={fadeIn("right", "tween", 0.2, 1)}
+                  className="sm:text-[80px] text-[30px]  tracking-[2px] sm:text-left text-right"
+                >
+                  CARE
                 </motion.h1>
               </div>
             </div>
-            <div className="w-full grid sm:grid-cols-12">
+            <div className="w-full grid sm:grid-cols-12 items-center space-x-3">
               <div className="col-span-8">
                 <motion.h1
                   variants={fadeIn("left", "tween", 0.2, 1)}
@@ -43,6 +113,21 @@ export default function Hero() {
                   WITH TECHOLOGY
                 </motion.h1>
               </div>
+              <motion.div
+                variants={fadeIn("down", "tween", 0.8, 1)}
+                className="col-span-4 w-full"
+              >
+                <div className="flex gap-6 ">
+                  <div className="relative flex flex-row">
+                    <div className="w-[50px] h-[50px] bg-black rounded-full "></div>
+                    <div className="w-[50px] h-[50px] bg-red-500 rounded-full absolute left-[70%] top-0"></div>
+                  </div>
+                  <div className="relative flex flex-row">
+                    <div className="w-[50px] h-[50px] bg-black rounded-full "></div>
+                    <div className="w-[50px] h-[50px] bg-red-500 rounded-full absolute left-[70%] top-0"></div>
+                  </div>
+                </div>
+              </motion.div>
             </div>
             <div className="flex justify-between items-center mt-[1%]">
               <h4>Your smile, Our passion</h4>
